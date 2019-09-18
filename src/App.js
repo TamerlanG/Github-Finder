5a0b32ca13a3fb224e1bbc73856c7129a1b086d8
TamerlanG <tamerlanalbehairy@gmail.com>
2019-09-18 19:32:58 +0600

    Fetch and add info from github api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
-
+import Users from "./components/users/Users";
+import axios from 'axios';
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    // Create a request to the github api
+    const response = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: response.data, loading: false });
+  }
+
 
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
