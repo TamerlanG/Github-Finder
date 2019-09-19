46a7cd19b4a8ed0d794f23da6e83a88b817fa46e
TamerlanG <tamerlanalbehairy@gmail.com>
2019-09-19 16:51:58 +0600

    Add view user functionality and view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search"
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
@@ -10,6 +11,7 @@ import axios from 'axios';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -21,6 +23,15 @@ class App extends Component {
       .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({ users: response.data.items, loading: false });
+  }
+
+  // Get a single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true })
+    const response = await axios
+      .get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: response.data, loading: false });
   }
 
   // Clear users from state
@@ -42,7 +53,7 @@ class App extends Component {
 
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -58,6 +69,12 @@ class App extends Component {
               )} />
 
               <Route exact path='/about' component={About} />
+              <Route
+                exact path='/user/:login'
+                render={props => (
+                  <User {...props} getUser={this.getUser} user={user} loading={loading} />
+                )}
+              />
             </Switch>
           </div>
         </div>
